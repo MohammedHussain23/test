f04a73fb52b41e7c0bed2dc6c664ef247fb49970
Mohammed Hussain H B <hbmdhussain@gmail.com>
2025-07-04 21:43:35 +0530

    adding the print statement to show results

--- test.py
+++ test.py
@@ -3,3 +3,4 @@ from my_math import math_operations
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 result = math_operations(a, b)
+print(f"Results of operations on {a} and {b}\n{result}")
